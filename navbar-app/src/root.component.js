11292f87cba6721baab0a5b371347f9e9f3a95ef
Laras Rasdiyani <61336325+larasras@users.noreply.github.com>
2023-09-17 20:01:46 +0700

    Add react-router-dom in navbar-app

--- navbar-app/src/root.component.js
+++ navbar-app/src/root.component.js
@@ -1,3 +1,20 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+// export default function Root(props) {
+//   return <section>{props.name} is mounted!</section>;
+// }
+import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+
+export default function Root() {
+  return (
+    <BrowserRouter>
+      <nav className="nav">
+        <Link to="/sign-up" className="link">
+          Page 1
+        </Link>
+        <Link to="/manage-item" className="link">
+          Page 2
+        </Link>
+      </nav>
+    </BrowserRouter>
+  );
 }
